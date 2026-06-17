0f29252cfb20d903107a0837f52aac6f9280d143
Lyncs <71156956+LyncsCwtsh@users.noreply.github.com>
2026-06-17 02:12:21 +0200

    1.1.0 (#1)
    * Update module.json
    
    * Rename daggerdice.json to daggerdice.js
    
    * Update Changelog.md
    
    * Update Changelog.md
    
    * Update Used Asset Credits
    
    * Delete images/esrtg
    
    * Add files via upload
    
    * Update Changelog.md
    
    * Add files via upload
    
    * Create delete
    
    * Create delete
    
    * Create delerwes
    
    * Add files via upload
    
    * Add files via upload
    
    * Delete daggerdice/faces/swordedglass/delete
    
    * Delete daggerdice/faces/swordedglass/dark/delerwes
    
    * Add files via upload
    
    * Add files via upload
    
    * Create deltete
    
    * Add files via upload
    
    * Delete daggerdice/faces/knightglass/light/deltete
    
    * Add files via upload
    
    * Delete daggerdice/faces/swordedglass/dark directory
    
    * Create deltee
    
    * Add files via upload
    
    * Delete daggerdice/faces/swordedglass/dark/deltee
    
    * Delete daggerdice/faces/knightglass/delete
    
    * Update Used Asset Credits
    
    * Delete module.zip
    
    * Add files via upload
    
    * Update Changelog.md
    
    * Update module.json
    
    * Delete module.zip
    
    * Add files via upload

--- Changelog.md
+++ Changelog.md
@@ -1,8 +1,16 @@
+## 1.1.0
+- NEW 2 Stained Glass inspired d12s:
+  - Castle Knights: with numbers (light and dark variant)
+  - 12 Swords till Midnight: the higher the number, the higher the number of swords (light and dark variant)
+- JSON/JS fix for Chrome
+
+![preview](images/stainedglasspreview.webp?raw=true)
+*Examples with metal and plastic as material*
 
 ## 1.0.0 Initial Release
 - D12s:
   - Arrowed (dark and light variant)
-  - Runic Warrior (set design, cutout and fill in Variant)
+  - Runic Warrior (set design, cutout and fill Variant)
   - Sundown
   - Thorns
 
--- daggerdice/scripts/daggerdice.js
+++ daggerdice/scripts/daggerdice.js
@@ -70,6 +70,77 @@ dice3d.addSystem({id:"arroweddark",name:"🗡️ Arrowed (Dark)"},false);
       system:"arrowedlight"
   });
 
+dice3d.addSystem({id:"knightglass2",name:"🗡️ Castle Knights (Light)"},false);
+
+  dice3d.addDicePreset({
+    type:"d12",
+    labels:[
+      'modules/daggerdice/faces/knightglass/light/d12-1.webp',
+      'modules/daggerdice/faces/knightglass/light/d12-2.webp',
+      'modules/daggerdice/faces/knightglass/light/d12-3.webp',
+      'modules/daggerdice/faces/knightglass/light/d12-4.webp',
+      'modules/daggerdice/faces/knightglass/light/d12-5.webp',
+      'modules/daggerdice/faces/knightglass/light/d12-6.webp',
+      'modules/daggerdice/faces/knightglass/light/d12-7.webp',
+      'modules/daggerdice/faces/knightglass/light/d12-8.webp',
+      'modules/daggerdice/faces/knightglass/light/d12-9.webp',
+      'modules/daggerdice/faces/knightglass/light/d12-10.webp',
+      'modules/daggerdice/faces/knightglass/light/d12-11.webp',
+      'modules/daggerdice/faces/knightglass/light/d12-12.webp'
+    ],
+    bumpMaps:[
+      'modules/daggerdice/faces/knightglass/d12-1b.webp',
+      'modules/daggerdice/faces/knightglass/d12-2b.webp',
+      'modules/daggerdice/faces/knightglass/d12-3b.webp',
+      'modules/daggerdice/faces/knightglass/d12-4b.webp',
+      'modules/daggerdice/faces/knightglass/d12-5b.webp',
+      'modules/daggerdice/faces/knightglass/d12-6b.webp',
+      'modules/daggerdice/faces/knightglass/d12-7b.webp',
+      'modules/daggerdice/faces/knightglass/d12-8b.webp',
+      'modules/daggerdice/faces/knightglass/d12-9b.webp',
+      'modules/daggerdice/faces/knightglass/d12-10b.webp',
+      'modules/daggerdice/faces/knightglass/d12-11b.webp',
+      'modules/daggerdice/faces/knightglass/d12-12b.webp'
+	 ],
+      system:"knightglass2"
+  });
+
+dice3d.addSystem({id:"knightglass",name:"🗡️ Castle Knights (Dark)"},false);
+
+  dice3d.addDicePreset({
+    type:"d12",
+    labels:[
+      'modules/daggerdice/faces/knightglass/d12-1.webp',
+      'modules/daggerdice/faces/knightglass/d12-2.webp',
+      'modules/daggerdice/faces/knightglass/d12-3.webp',
+      'modules/daggerdice/faces/knightglass/d12-4.webp',
+      'modules/daggerdice/faces/knightglass/d12-5.webp',
+      'modules/daggerdice/faces/knightglass/d12-6.webp',
+      'modules/daggerdice/faces/knightglass/d12-7.webp',
+      'modules/daggerdice/faces/knightglass/d12-8.webp',
+      'modules/daggerdice/faces/knightglass/d12-9.webp',
+      'modules/daggerdice/faces/knightglass/d12-10.webp',
+      'modules/daggerdice/faces/knightglass/d12-11.webp',
+      'modules/daggerdice/faces/knightglass/d12-12.webp'
+    ],
+    bumpMaps:[
+      'modules/daggerdice/faces/knightglass/d12-1b.webp',
+      'modules/daggerdice/faces/knightglass/d12-2b.webp',
+      'modules/daggerdice/faces/knightglass/d12-3b.webp',
+      'modules/daggerdice/faces/knightglass/d12-4b.webp',
+      'modules/daggerdice/faces/knightglass/d12-5b.webp',
+      'modules/daggerdice/faces/knightglass/d12-6b.webp',
+      'modules/daggerdice/faces/knightglass/d12-7b.webp',
+      'modules/daggerdice/faces/knightglass/d12-8b.webp',
+      'modules/daggerdice/faces/knightglass/d12-9b.webp',
+      'modules/daggerdice/faces/knightglass/d12-10b.webp',
+      'modules/daggerdice/faces/knightglass/d12-11b.webp',
+      'modules/daggerdice/faces/knightglass/d12-12b.webp'
+	 ],
+      system:"knightglass"
+  });
+
+
  dice3d.addSystem({id:"thorns",name:"🗡️ Thorns"},false);
 
   dice3d.addDicePreset({
@@ -244,6 +315,77 @@ dice3d.addSystem({id:"arroweddark",name:"🗡️ Arrowed (Dark)"},false);
 	 ],
       system:"sundown"
   });
+
+   dice3d.addSystem({id:"swordglass",name:"🗡️ 12 Swords till Midnight (Light)"},false);
+
+  dice3d.addDicePreset({
+    type:"d12",
+    labels:[
+      'modules/daggerdice/faces/swordedglass/d12-1.webp',
+      'modules/daggerdice/faces/swordedglass/d12-2.webp',
+      'modules/daggerdice/faces/swordedglass/d12-3.webp',
+      'modules/daggerdice/faces/swordedglass/d12-4.webp',
+      'modules/daggerdice/faces/swordedglass/d12-5.webp',
+      'modules/daggerdice/faces/swordedglass/d12-6.webp',
+      'modules/daggerdice/faces/swordedglass/d12-7.webp',
+      'modules/daggerdice/faces/swordedglass/d12-8.webp',
+      'modules/daggerdice/faces/swordedglass/d12-9.webp',
+      'modules/daggerdice/faces/swordedglass/d12-10.webp',
+      'modules/daggerdice/faces/swordedglass/d12-11.webp',
+      'modules/daggerdice/faces/swordedglass/d12-12.webp'
+    ],
+    bumpMaps:[
+      'modules/daggerdice/faces/swordedglass/d12-1b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-2b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-3b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-4b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-5b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-6b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-7b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-8b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-9b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-10b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-11b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-12b.webp'
+	 ],
+      system:"swordglass"
+  });
+
+   dice3d.addSystem({id:"swordglass2",name:"🗡️ 12 Swords till Midnight (Dark)"},false);
+
+  dice3d.addDicePreset({
+    type:"d12",
+    labels:[
+      'modules/daggerdice/faces/swordedglass/dark/d12-1.webp',
+      'modules/daggerdice/faces/swordedglass/dark/d12-2.webp',
+      'modules/daggerdice/faces/swordedglass/dark/d12-3.webp',
+      'modules/daggerdice/faces/swordedglass/dark/d12-4.webp',
+      'modules/daggerdice/faces/swordedglass/dark/d12-5.webp',
+      'modules/daggerdice/faces/swordedglass/dark/d12-6.webp',
+      'modules/daggerdice/faces/swordedglass/dark/d12-7.webp',
+      'modules/daggerdice/faces/swordedglass/dark/d12-8.webp',
+      'modules/daggerdice/faces/swordedglass/dark/d12-9.webp',
+      'modules/daggerdice/faces/swordedglass/dark/d12-10.webp',
+      'modules/daggerdice/faces/swordedglass/dark/d12-11.webp',
+      'modules/daggerdice/faces/swordedglass/dark/d12-12.webp'
+    ],
+    bumpMaps:[
+      'modules/daggerdice/faces/swordedglass/d12-1b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-2b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-3b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-4b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-5b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-6b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-7b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-8b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-9b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-10b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-11b.webp',
+      'modules/daggerdice/faces/swordedglass/d12-12b.webp'
+	 ],
+      system:"swordglass2"
+  });
+
 
 	dice3d.addTexture("greywolf", {
 	    name: "🗡️ Greywolf",
